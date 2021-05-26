f79445d383794dbdb99945d563d36ec33aa728ad
Varun Gautam <64063924+gautamvarun@users.noreply.github.com>
2021-05-26 18:07:10 +0530

    Update Stackmain.java

--- Stackmain.java
+++ Stackmain.java
@@ -2,20 +2,20 @@
 import java.util.*;
 public class Stackmain
 {
-public void display(Stack st1,Stack st2)
+public void display(Stack s1,Stack s2)
 {
-if(st1.count()==st2.count())
+if(s1.count()==s2.count())
 {
-System.out.println("Total Numbers of Elements in both stack is : " + st2.count());
+System.out.println("Both stacks have " + s2.count()+ " elements");
 }
 }
 public static void main(String []args)
 {
 Scanner sc=new Scanner(System.in);
-Stack s1=new Stack(20);
-Stack s2=new Stack(20);
-int ch,data;
-StackImp si=new StackImp();
+Stack s1=new Stack(10);
+Stack s2=new Stack(10);
+int choice,data;
+Stackmain s=new Stackmain();
 while(true)
 {
 System.out.println("\n1. Push into Stack 1");
@@ -23,28 +23,28 @@ System.out.println("2. Push into Stack 2");
 System.out.println("3. Pop from Stack 1");
 System.out.println("4. Pop from Stack 1");
 System.out.println("Enter Your Choice : ");
-ch=sc.nextInt();
-switch(ch)
+choice=sc.nextInt();
+switch(choice)
 {
 case 1:
 System.out.println("Enter The Data");
 data=sc.nextInt();
-s1.push(data);
-si.display(s1,s2);
+s.push(data);
+s.display(s1,s2);
 break;
 case 2:
 System.out.println("Enter The Data");
 data=sc.nextInt();
 s2.push(data);
-si.display(s1,s2);
+s.display(s1,s2);
 break;
 case 3:
 System.out.println(s1.pop());
-si.display(s1,s2);
+s.display(s1,s2);
 break;
 case 4:
 System.out.println(s2.pop());
-si.display(s1,s2);
+s.display(s1,s2);
 break;
 default:
 }
